a3eace7c202160cdf54281e9b89547db95db3d96
DanielTimbie <44413655+DanielTimbie@users.noreply.github.com>
2023-05-14 21:25:28 -0500

    Uploaded new data

--- Ft_data.py
+++ Ft_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from fourier_stuff import StandardFourier
 
-tg_dir = '/Users/GuestUser/Documents/Labwork/Memristor/Data Analysis Codes/Measurements 050123/FIB3_U8_3_noise_7_low_drift_LP6dB6dBHz_Integ1_retentiondata.csv'
+tg_dir = '/Users/daniel/Documents/Northwestern 2020-2023/Labwork/Memristor/Data_Analysis_Codes_Memristor/data/Measurements 050123/FIB3_U8_3_noise_7_low_drift_LP6dB6dBHz_Integ1_retentiondata.csv'
 
 rvals = np.loadtxt(tg_dir, delimiter=',', skiprows=1, usecols=0, dtype=float)
 #tval = np.loadtxt(tg_dir, delimiter=',', skiprows=1, usecols=2, dtype=float)[3] - np.loadtxt(tg_dir, delimiter=',', skiprows=1, usecols=2, dtype=float)[2]
@@ -15,7 +15,7 @@ data = StandardFourier(cvals,fr)
 plt.loglog(data[0], data[1])
 plt.title('FIB3_U8_3: Noise Spectra')
 plt.ylabel('')
-plt.xlabel('Freq (Hz)')
+plt.xlabel('Frequency (Hz)')
 #plt.xlim(-0.1,1)
 plt.show()
 
